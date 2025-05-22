918eac59d74539e115b84ff7fb12333b20434591
Kushagra <kushagra100chhabra@gmail.com>
2025-05-22 21:15:20 +0530

    hu

--- vite-project/src/components/Games.jsx
+++ vite-project/src/components/Games.jsx
@@ -6,31 +6,33 @@ import image4 from '../assets/images/life4.jpg'
 
 export default function Games() {
 
-     let  data = [
-            {title : image1 },
-            {title : image2},
-            {title : image3},
-            {title : image4},
-        ];
+    let data = [
+        { title: image1 },
+        { title: image2 },
+        { title: image3 },
+        { title: image4 },
+    ];
 
-  return (
-    <div className=' md:w-full w-150 bg-blue-800 text-white'>
-        
-        <div className=' text-center py-20' >
-            <h1 className=' text-7xl font-bold'>Life is Strange Games</h1>
-            <h1 className=' py-10 text-2xl'>A series of thrilling supernatural mysteries to experience.</h1>
-        </div>
+    return (
+        <div className=' md:w-full w-150 bg-blue-800 text-white'>
 
-        <div className=' flex text-center justify-center gap-10 py-0'>
-            {
-                    data.map((item,key) =>(
-                        <div>
-                            <img className='px-5 w-50 h-40 md:h-70 md:w-70' src={item.title} alt="" />
+            <div className=' text-center py-20' >
+                <h1 className=' text-7xl font-bold'>Life is Strange Games</h1>
+                <h1 className=' py-10 text-2xl'>A series of thrilling supernatural mysteries to experience.</h1>
+            </div>
+
+            <div className='  md:flex text-center  md:text-center md:justify-center gap-10 py-0'>
+                {
+                    data.map((item, key) => (
+                        <div className=' flex justify-center py-2 '>
+                            <div className='  w-100 h-80 md:h-70 md:w-70 '>
+                                <img className=' w-100 h-80 md:h-70 md:w-70  rounded-2xl ' src={item.title} alt="" />
+                            </div>
                         </div>
                     ))
                 }
-        </div>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
